63d0da757ea06e5ba759ea3cf81a394212be84f3
EtienneR <etienne.rouzeaud@free.fr>
2014-06-23 21:23:42 +0200

    update

--- index.php
+++ index.php
@@ -1,27 +1,38 @@
 <?php $title = 'Convert image to webp image'; ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
 	<meta charset="UTF-8" />
 	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
 <body>
-	<h1><?php echo $title; ?></h1>
-	<p>/!\ : Working only on Chrome and Opera - <em>jpg</em>, <em>gif</em> or <em>png</em> files accepted<p>
-	<form action="index.php" method="POST" enctype='multipart/form-data'>
-		<input type="file" name="file" required />
-		<input type="submit" value="Uploader" />
-	</form>
-</body>
-</html>
+
+	<div class="container">
+
+		<h1><?php echo $title; ?></h1>
+		<p>/!\ : Working only on Chrome and Opera - <em>jpg</em>, <em>gif</em> or <em>png</em> files accepted<p>
+		<form action="index.php" method="POST" enctype='multipart/form-data' role="form">
+			<div class="form-group">
+				<input type="file" name="file" required />
+			</div><!-- // .form-group -->
+			<button type="submit" class="btn btn-primary">Upload</button>
+		</form>
+
 
 
 <?php
 if (isset($_FILES['file'])){
+?>
+<div class="row">
+<?php
+
 	//var_dump($_FILES);
 	$error 	  = $_FILES['file']['error'];
 	$filename = $_FILES['file']['name'];
 	//1 Mo = 2^20 octets = 1048576 octets
 	$max_size = 2097152;
+	$filename_orignal = 'img/' . $filename;
 
 	if ($error == 0){
 
@@ -49,8 +60,17 @@ if (isset($_FILES['file'])){
 
 			$name = substr($name, 0, -3).'webp';
 			imagewebp($image, $name);
+			$orignial_ko = number_format($_FILES['file']['size'] / 1024, 2);
+			$webp_ko = number_format(filesize($name) / 1024, 2);
 
-			echo 'Webp image : <br /><img src=' . $name . ' alt=' . $name .' />';
+			echo '<div class="col-xs-6 col-md-6">';
+			echo 'Original file size : ' . $orignial_ko . ' Ko  <br />';
+			echo '<a href="' . $filename_orignal . '" class="thumbnail"><img src=' . $filename_orignal . ' alt="" /></a>';
+			echo '</div>';
+			echo '<div class="col-xs-6 col-md-6">';
+			echo 'Web file size ' . $webp_ko . ' Ko <br />';
+			echo '<a href="' . $name . '" class="thumbnail"><img src=' . $name . ' alt="" /></a>';
+			echo '</div>';
 		}
 		else{
 			echo 'Bad extension';
@@ -64,7 +84,13 @@ if (isset($_FILES['file'])){
 	else{
 		echo 'Error';
 	}
-}
-
-
 ?>
+</div><!-- // .row -->
+<?php
+}
+?>
+
+	</div><!-- // .container -->
+
+</body>
+</html>
